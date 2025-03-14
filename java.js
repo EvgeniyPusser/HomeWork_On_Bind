46e5365062f36869b1bf1edbdbb313f47bff6b37
Evgeniy <evgeniipusser88@gmail.com>
2025-03-15 00:06:47 +0200

    at last - name of function not the executing

--- java.js
+++ java.js
@@ -19,9 +19,28 @@
 //   }
 // }
 
+// class Deferred {
+//   constructor() {
+//     this.chain = [];
+//   }
+
+//   then(callback) {
+//     this.chain.push(callback);
+//     return this; // Allow chaining
+//   }
+
+//   resolve(value) {
+//     let currentValue = value; // Initialize with the input value
+//     this.chain.forEach((callback) => {
+//       currentValue = callback(currentValue); // Update value through the chain
+//     });
+//   }
+// }
+
 class Deferred {
   constructor() {
     this.chain = [];
+    this.value = undefined;
   }
 
   then(callback) {
@@ -30,12 +49,19 @@ class Deferred {
   }
 
   resolve(value) {
-    let currentValue = value; // Initialize with the input value
-    this.chain.forEach(function (callback) {
-      currentValue = callback(currentValue); // Update value through the chain
-    });
+    this.value = value; // Set initial value
+
+    // Mediate variable storing the correctly bound function
+    const methodRef = this.runCallbackName.bind(this);
+
+    this.chain.forEach(methodRef); // Passing only the function name
+  }
+
+  runCallbackName(callback) {
+    this.value = callback(this.value); // Update value with each callback result
   }
 }
+
 
 const d = new Deferred();
 d.then(function (res) {
@@ -69,3 +95,64 @@ function greet(age, country) {
 const greetAlice = greet.myBind(person, 25);
 
 greetAlice("USA");
+
+const first = (callback) => {
+  // Как будто бы запрос к API
+  setTimeout(() => {
+    console.log(1);
+    callback();
+  }, 500);
+};
+
+const second = () => {
+  console.log(2);
+};
+
+first(second);
+
+// T.get("search/tweets", params, (err, data, response) => {
+//   if (!err) {
+//     // Происходит какая-то магия
+//   } else {
+//     console.log(err);
+//   }
+// });
+
+// const params = {
+//   q: "JavaScript", // Search query
+//   count: 10, // Number of tweets to return
+//   lang: "en", // Language filter
+// };
+
+// if (!err) {
+//   console.log(data); // Logs the response object
+// }
+// if (!err) {
+//   data.statuses.forEach((tweet) => {
+//     console.log(tweet.text);
+//   });
+// }
+
+// const { TwitterApi } = require("twitter-api-v2");
+
+// const client = new TwitterApi({
+//   appKey: "YOUR_API_KEY",
+//   appSecret: "YOUR_API_SECRET",
+//   accessToken: "YOUR_ACCESS_TOKEN",
+//   accessSecret: "YOUR_ACCESS_SECRET",
+// });
+
+// async function fetchTweets() {
+//   try {
+//     const tweets = await client.v2.search("JavaScript", { max_results: 5 });
+//     tweets.data.forEach((tweet, index) =>
+//       console.log(`${index + 1}: ${tweet.text}\n`)
+//     );
+//   } catch (error) {
+//     console.error("Error fetching tweets:", error);
+//   }
+// }
+
+// fetchTweets();
+
+
